65a58e3d232245f2a4c8d25943d919ad266d3b26
“Erno <ernohilarion@MacBook-Air-de-Erno.local>
2024-05-10 11:00:23 +0200

    sidebar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import Footer from './components/Footer/Footer'
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,46 @@
+import AboutUsPage from '../../pages/AboutUsPage/AboutUsPage'
+import AllTracksPage from '../../pages/AllTracksPage/AllTracksPage'
+import LikedTracksPage from '../../pages/LikedTracksPage/LikedTracksPage'
 import './Sidebar.css'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useState } from 'react';
 
-const Sidebar = () => { }
+const Sidebar = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Launch
+            </Button>
+
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>HACK TRACKS</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <ul>
+                        <li>
+                            <Link to={'/all-tracks'}>All Tracks</Link>
+
+                        </li>
+
+                        <li>
+                            <Link to={'/liked-tracks'}>Liked Tracks</Link>
+                        </li>
+
+                        <li>
+                            <Link to={'/about-us'}>About Us</Link>
+                        </li>
+                    </ul>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    )
+}
 
 export default Sidebar
