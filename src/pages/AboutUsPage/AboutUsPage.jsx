887f479ed9f1dd21357cc9ae0e6a2d1c4f6ed20d
“Erno <ernohilarion@MacBook-Air-de-Erno.local>
2024-05-10 14:29:40 +0200

    about us and details

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,9 +1,46 @@
 import { Link } from "react-router-dom"
 import './AboutUsPage.css'
+import { Container, Button, Card, Row, Col } from "react-bootstrap"
+import SOKOBAN from '../../assets/SOKOBAN.png'
+
+
+
+
+
 function AboutUsPage() {
     return (
         <div className='AboutUsPage'>
-            <h1>Soy la AboutUsPage</h1>
+            <Container>
+                <Row>
+
+                    <Col md={{ span: 6 }}>
+                        <Card >
+                            <Card.Img variant="top" src={SOKOBAN} />
+                            <Card.Body>
+                                <Card.Title>ERNO</Card.Title>
+                                <Card.Text>
+                                    CEO
+                                </Card.Text>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col md={{ span: 6 }}>
+                        <Card >
+                            <Card.Img variant="top" src={SOKOBAN} />
+                            <Card.Body>
+                                <Card.Title>ADRIAN</Card.Title>
+                                <Card.Text>
+                                    CEO
+                                </Card.Text>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                </Row>
+            </Container>
         </div>
     )
 }
